7f2834ff39f2e2e7ea087a15c6bab17db18883b4
isaac <isaacgan@wezhuiyi.com>
2019-01-01 15:58:33 +0800

    [feature] 添加合并模式

--- bin/icsdr.js
+++ bin/icsdr.js
@@ -6,32 +6,62 @@ const store = require('../lib/store');
 const argvs = process.argv.slice(2);
 const isNormalMode = !(argvs[0] && argvs[0].startsWith('-'));
 const isReadDefaultConfig = !argvs.length;
+const options = {};
 const config = {};
 
 program
   .version(require('../package.json').version)
   .option('-i --init [filePath]', 'init variables file')
-  .option('-l --list [url]', 'catalog url list', function (list) {config.catcalogs = list})
-  .option('-s --search [comic]', 'search comic', function (comic) {config.search = comic})
-  .parse(process.argv)
+  .option('-l --list [url]', 'catalog url list', (list) => {
+    config.catalogs = list;
+  })
+  .option('-s --search [comic]', 'search comic', (comicName) => {
+    options.isSearch = true;
+    config.comicName = comicName;
+  })
+  .option('-S --start [start chapter]', 'name of start-chapter', (startChapter) => {
+    options.isRange = true;
+    console.log(startChapter);
+    config.startChapter = startChapter;
+  })
+  .option('-E --end [end chapter]', 'name of end-chapter', (endChapter) => {
+    options.isRange = true;
+    config.endChapter = endChapter;
+  })
+  .option('-n --num [vol num]', 'volume number(chapter or picture)', (num) => {
+    const unit = num.charAt(num.length - 1);
+    const realNum = Number.parseInt(num);
+    const defaultUnit = realNum > 30 ? 'p' : 'c';
+    const realUnit = ['p', 'c'].includes(unit) ? unit : defaultUnit;
+    config.volSize = {
+      num: realNum,
+      unit: realUnit
+    };
+  })
+  .option('-m --merge [comic dirname]', 'comic dirname', (comicName) => {
+    options.isMerge = true;
+    config.comicName = comicName;
+  })
+  .parse(process.argv);
 
 if (program.init) {
   spider.init();
 } else {
   if (isNormalMode) {
-    config.catcalogs = argvs;
+    config.catalogs = argvs;
   }
   
   if (isReadDefaultConfig) {
     config = spider.readConfig();
   }
 
-  store.set({ config });
+  store.set({ options, ...config });
 
-  if (config.search) {
-    spider.search(config.search);
+  if (options.isSearch) {
+    spider.search(config.comicName);
+  } else if (options.isMerge) {
+    spider.merge();
   } else {
-    spider.run(config.catcalogs);
+    spider.run(config.catalogs);
   }
 }
-
